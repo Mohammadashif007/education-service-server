8ac9fb0f5374b0bc47a227e6d981e8fadf63389c
Mohammad Ashif <Mohammad Ashif>
2024-05-15 14:15:09 +0600

    some error fixed

--- index.js
+++ index.js
@@ -8,9 +8,8 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri =
-`mongodb+srv://${process.env.DB_user}:${process.env.DB_pass}@cluster0.u69fsfj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_user}:${process.env.DB_pass}@cluster0.u69fsfj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -127,6 +126,14 @@ async function run() {
             const result = await bookingCollections.updateOne(query, updateDoc);
             res.send(result);
         });
+
+        app.delete("/bookings/:id", async(req, res) => {
+            const id = req.params.id;
+            console.log(id);
+            const query = {_id: new ObjectId(id)};
+            const result = await bookingCollections.deleteOne(query);
+            res.send(result);
+        })
 
         //! Review collections
         app.get("/reviews", async (req, res) => {
