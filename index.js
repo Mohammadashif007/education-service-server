d2dd26a6156658a99186d15cb65878fea05b4fb3
Mohammad Ashif <Mohammad Ashif>
2024-05-15 11:35:54 +0600

    some error fixed

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         const courseCollections = client
             .db("educationDB")
@@ -150,7 +150,7 @@ async function run() {
         // await client.close();
     }
 }
-run().catch(console.dir);
+// run().catch(console.dir);
 
 app.get("/", (req, res) => {
     res.send("server is open");
